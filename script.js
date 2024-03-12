db8f7487c5aea8c44a9c488fc1c0a0829d0dc6fe
capital1umar <musaumarfaruq17@gmail.com>
2024-03-11 22:33:16 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const mobileNav = document.getElementById("mobileNav");
+const openNav = document.querySelector(".openNav");
+
+function openNavigation() {
+  const openNav = document.querySelector(".openNav");
+  openNav.style.display = "flex";
+  mobileNav.style.display = "none";
+}
+
+function closeNavigation() {
+  openNav.style.display = "none";
+  mobileNav.style.display = "block";
+}
